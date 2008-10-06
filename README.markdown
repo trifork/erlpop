782cf13219edfbb63a07c2428fe2ed3db98ac73f
Harish Mallipeddi <harish.mallipeddi@gmail.com>
2008-10-06 17:24:22 +0800

    Added epop_message to parse retrieved email messages

--- README.markdown
+++ README.markdown
@@ -6,10 +6,11 @@ erlpop
 Author: *Harish Mallipeddi* (harish.mallipeddi@gmail.com)
 
 ### Changes ###
+    2009-10-06 Added `epop_message` to parse retrieved email messages.
     2008-09-10 Added SSL support (epop_client can be now be used with services like GMail which require SSL).
 
-## Usage ##
+### Usage ###
 
     > {ok, Client} = epop_client:connect("yourname@gmail.com", "yourpassword", [{addr,"pop.gmail.com"},{port,995},ssl]).
     > epop_client:stat(Client).
-    > epop_client:retr(Client, 1).
+    > epop_client:retrieve(Client, 1).
---
+++ src/epop_message.erl
@@ -0,0 +1,52 @@
+-module(epop_message).
+-author("Harish Mallipeddi <harish.mallipeddi@gmail.com>").
+
+%%%---------------------------------------------------------------------
+%%% File    : epop_message.erl
+%%% Created : 17 Sep 2008 by harish.mallipeddi@gmail.com
+%%% Function: Utility to parse email messages
+%%% ====================================================================
+%%% The contents of this file are subject to the Erlang Public License
+%%% License, Version 1.1, (the "License"); you may not use this file
+%%% except in compliance with the License. You may obtain a copy of the
+%%% License at http://www.erlang.org/EPLICENSE
+%%%
+%%% Software distributed under the License is distributed on an "AS IS"
+%%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%%% the License for the specific language governing rights and limitations
+%%% under the License.
+%%%
+%%%---------------------------------------------------------------------
+
+-export([parse/1, find_header/2]).
+
+parse(Message) ->
+    {_, Boundary, _} = regexp:first_match(Message, "\r\n\r\n"),
+    Header = string:substr(Message, 1, Boundary - 1),
+    Body = string:substr(Message, Boundary + 4),
+    Headers = parse_headers(Header),
+    {message, Headers, Body}.
+
+parse_headers(Header) ->    
+    %% do "unfolding" first
+    %% read more about unfolding long header fields here - http://www.faqs.org/rfcs/rfc2822.html
+    {ok, Chunks} = regexp:split(Header, "\r\n[ |\t]"),
+    UnfoldedHeader = string:join(Chunks, " "),
+    
+    {ok, RawHeaders} = regexp:split(UnfoldedHeader, "\r\n"),
+    %io:format("raw headers = ~n~p~n", [RawHeaders]),
+    lists:map(fun parse_header/1, RawHeaders).
+
+parse_header(RawHeader) ->
+    {_, Boundary, _} = regexp:first_match(RawHeader, ":"),
+    HeaderName = string:strip(string:substr(RawHeader, 1, Boundary - 1)),
+    HeaderVal = string:strip(string:substr(RawHeader, Boundary + 1)),
+    {header, HeaderName, HeaderVal}.
+
+%% find the header given the name from the headers list
+find_header([], _) ->
+    {error, not_found};
+find_header([{header, HeaderName, HeaderValue}|_Rest], Key) when HeaderName =:= Key ->
+    {ok, HeaderValue};
+find_header([_|Rest], Key) ->
+    find_header(Rest, Key).
