8668d75a1eb61e85f82a86ddc47cb76cf8c4b2bf
Nico Hoogervorst <16443840+nico-amsterdam@users.noreply.github.com>
2019-05-30 22:02:38 +0200

    License text

--- src/epop_address.erl
+++ src/epop_address.erl
@@ -5,18 +5,19 @@
 %%% File    : epop_address.erl
 %%% Function: Utility to parse email addresses and address lists
 %%% ====================================================================
-%%% The contents of this file are subject to the Erlang Public License
-%%% License, Version 1.1, (the "License"); you may not use this file
-%%% except in compliance with the License. You may obtain a copy of the
-%%% License at http://www.erlang.org/EPLICENSE
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
 %%%
-%%% Software distributed under the License is distributed on an "AS IS"
-%%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%%% the License for the specific language governing rights and limitations
-%%% under the License.
+%%%      http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
 %%%
 %%%---------------------------------------------------------------------
-
 %%% Purpose: Email address list parser.
 %%% Status:
 %%% Is complete enough to be better than nothing (and, presumably,
